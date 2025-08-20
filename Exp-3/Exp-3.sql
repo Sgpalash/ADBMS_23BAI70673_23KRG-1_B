f29cee139bf6c8e7e3dfba9187c1e83001badbce
Palash Mathur <mathurpalash5@gmail.com>
2025-08-20 19:30:37 +0530

    Exp-3
    On 20th Aug 2025

---
+++ Exp-3/Exp-3.sql
@@ -0,0 +1,133 @@
+SHOW Databases;
+use ADBMS;
+
+/*--------------Exp-3: SUB-QUERIES -----------------
+
+DEF: SUB-Query / NESTED Query 
+    Q1: OUTER QUERY / MAIN QUERY (Q2 (Q3)) - INNER QUERIES
+
+    EXECUTION ORDER:
+    Q3>Q2>Q1
+
+    TYPES OF SUB QUERIES
+        1. SCALER SQ --> Returns Single Output;
+        2. MULTI - VALUED / MULTI - ROW -> RETURNS MULTIPLE ROWS 
+            OPERATORS: IN, NOT IN, ANY(OR), ALL(AND)
+        3. SELF-CONTAINED SQ: SQ WHICH HAVE NO DEPENDENCY ON MAIN QUERY
+            Q1(Q2) --> we can run q2 alone only.
+        4. CO-RELATED SQ: INNER QUERY WILL BE DEPENDENT ON OUTER QUERY
+            Q1(Q2) --> we cant run q2 alone only.
+
+    PLACEMENT OF SUB QUERIES:
+        1. WE CAN SQ WITH WHERE CLAUSE STATEMENT
+        */
+
+SELECT * FROM author;
+
+SELECT * FROM emp;
+
+SELECT AUTHOR_ID FROM author IN ()
+
+
+CREATE TABLE Employee(EmpID INT, Name VARCHAR(20));
+INSERT INTO Employee VALUES(2,"A");
+INSERT INTO Employee VALUES(4,"B");
+INSERT INTO Employee VALUES(4,"C");
+INSERT INTO Employee VALUES(6,"D");
+INSERT INTO Employee VALUES(6,"E");
+INSERT INTO Employee VALUES(7,"F");
+INSERT INTO Employee VALUES(8,"G");
+INSERT INTO Employee VALUES(8,"H");
+
+SELECT * FROM Employee
+
+SELECT MAX(EmpID) FROM Employee WHERE EmpID NOT IN 
+(
+    SELECT EmpID FROM Employee GROUP BY EmpID HAVING COUNT(EmpID) > 1
+);
+
+
+CREATE TABLE TBL_PRODUCTS
+(
+	ID INT PRIMARY KEY,
+	NAME VARCHAR(50),
+	DESCRIPTION VARCHAR(250) 
+);
+
+CREATE TABLE TBL_PRODUCTSALES
+(
+    ID INT PRIMARY KEY,
+    PRODUCT_ID INT,
+    UNITPRICE INT,
+    QUANTITYSOLD INT,
+    FOREIGN KEY (PRODUCT_ID) REFERENCES TBL_PRODUCTS(ID)
+)
+
+INSERT INTO TBL_PRODUCTS VALUES (1,"TV","52 INCH BLACK COLOR LCD TV");
+INSERT INTO TBL_PRODUCTS VALUES (2,"LAPTOP","VERY THIIN BLACK COLOR ACER LAPTOP");
+INSERT INTO TBL_PRODUCTS VALUES (3,"DESKTOP","HP HIGH PERFORMANCE DESKTOP");
+
+
+INSERT INTO TBL_PRODUCTSALES VALUES (1,3,450,5);
+INSERT INTO TBL_PRODUCTSALES VALUES (2,2,250,7);
+INSERT INTO TBL_PRODUCTSALES VALUES (3,3,450,4);
+INSERT INTO TBL_PRODUCTSALES VALUES (4,3,450,9);
+
+
+SELECT *FROM TBL_PRODUCTS
+SELECT *FROM TBL_PRODUCTSALES
+
+SELECT 
+    NAME,
+    (SELECT SUM(QUANTITYSOLD) FROM TBL_PRODUCTSALES WHERE PRODUCT_ID = P.ID) AS QUANTITYSOLD_SUM
+FROM TBL_PRODUCTS P;
+
+SELECT Name, 
+(
+    SELECT SUM(QUANTITYSOLD) FROM TBL_PRODUCTSALES WHERE PRODUCT_ID = TBL_PRODUCT.ID
+)AS QUANTITYSOLD_SUM FROM TBL_PRODUCTS;
+
+
+CREATE TABLE FootballParticipants (
+    ID INT PRIMARY KEY,
+    Name VARCHAR(50),
+    Email VARCHAR(100)
+);
+
+CREATE TABLE HockeyParticipants (
+    ID INT PRIMARY KEY,
+    Name VARCHAR(50),
+    Email VARCHAR(100)
+);
+
+
+
+
+INSERT INTO FootballParticipants (Name, Email)
+VALUES
+(1,'John', 'john.doe@example.com'),
+(2,'Jane', 'jane.smith@example.com'),
+(3,'Michael', 'michael.brown@example.com'),
+(4,'Emily', 'emily.davis@example.com'),
+(5,'David', 'david.wilson@example.com');
+
+
+INSERT INTO HockeyParticipants (Name, Email)
+VALUES
+(1,'John', 'john.doe@example.com'),
+(2,'Patricia', 'patricia.taylor@example.com'),
+(3, 'Michael', 'michael.brown@example.com'),
+(4,'Emily', 'emily.davis@example.com'),
+(5,'Kevin', 'kevin.martinez@example.com');
+
+
+SELECT *FROM  FootballParticipants
+SELECT *FROM  HockeyParticipants
+
+
+
+
+
+
+
+
